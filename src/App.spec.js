1a825dbff64776ef44a6139aaa41e45a5c3f5421
Robin Wieruch <wrobin@gmx.net>
2019-06-25 19:21:01 +0200

    finish tests

--- src/App.spec.js
+++ src/App.spec.js
@@ -1,42 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { act } from 'react-dom/test-utils';
+import { act } from 'react-dom/test-utils'; // React Test Utils
 import renderer from 'react-test-renderer'; // Jest
+import { render, fireEvent } from '@testing-library/react'; // React Testing Library
 
 import { withProfiler } from 'jest-react-profiler';
 
 import App from './App';
 
-const AppWithProfiler = withProfiler(App);
-
 describe('App', () => {
-  let container;
-
-  beforeEach(() => {
-    container = document.createElement('div');
-    document.body.appendChild(container);
-  });
-
-  afterEach(() => {
-    document.body.removeChild(container);
-    container = null;
-  });
-
   test('renders App', () => {
     const component = renderer.create(<App title="Hello World" />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
-  // Doesn't commit ...
+  test('Jest', () => {
+    const AppWithProfiler = withProfiler(App);
 
-  // test('Jest', () => {
-  //   renderer.create(<AppWithProfiler title="Hello World" />);
+    renderer.create(<AppWithProfiler title="Hello World" />);
 
-  //   expect(AppWithProfiler).toHaveCommittedTimes(1);
-  // });
+    // expect(AppWithProfiler).toHaveCommittedTimes(1); // Doesn't work with Jest Renderer
+  });
 
   test('React Utils', () => {
+    const AppWithProfiler = withProfiler(App);
+
+    const container = document.createElement('div');
+    document.body.appendChild(container);
+
     act(() => {
       ReactDOM.render(
         <AppWithProfiler title="Hello World" />,
@@ -52,11 +44,38 @@ describe('App', () => {
 
     act(() => {
       button.dispatchEvent(
-        new MouseEvent('click', { bubbles: true })
+        new MouseEvent('click', {
+          bubbles: true,
+        })
       );
     });
 
     expect(paragraph.textContent).toBe('1');
-    // expect(AppWithProfiler).toHaveCommittedTimes(2); // doesn't work
+    // Not intuitive API
+    // Source: Verifies that the test profiler component committed a specific number of times **__since__** the last time the matcher was called.
+    // My assumption would have been that the commited times would be only incremented with every commit
+    expect(AppWithProfiler).toHaveCommittedTimes(1);
+  });
+
+  test('React Testing Library', () => {
+    const AppWithProfiler = withProfiler(App);
+
+    const { container } = render(
+      <AppWithProfiler title="Hello World" />
+    );
+
+    const paragraph = container.querySelector('p');
+    const button = container.querySelector('button');
+
+    expect(paragraph.textContent).toBe('0');
+    expect(AppWithProfiler).toHaveCommittedTimes(1);
+
+    fireEvent.click(button);
+
+    expect(paragraph.textContent).toBe('1');
+    // Not intuitive API
+    // Source: Verifies that the test profiler component committed a specific number of times **__since__** the last time the matcher was called.
+    // My assumption would have been that the commited times would be only incremented with every commit
+    expect(AppWithProfiler).toHaveCommittedTimes(1);
   });
 });
